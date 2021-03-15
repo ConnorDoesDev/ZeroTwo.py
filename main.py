9c13ed8aa6097d152811dd1dbb57cfdf05876f06
ZeroTwo <halil_ismail@yahoo.com>
2021-03-15 17:46:43 +0100

    add bruh prefix

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from config import *
 import os
 
 client = commands.Bot(
-    command_prefix = ["py "], 
+    command_prefix = ["py ", "bruh "], 
         case_insensitive = True, 
         intents = discord.Intents.all(),
         allowed_mentions = discord.AllowedMentions(everyone=False, roles=False),
